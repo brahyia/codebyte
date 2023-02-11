2ca54492caa9c341ac55568f3362423be20da813
BenAfairi <benedicta_agyemangbrantuo@student.uml.edu>
2023-02-11 17:23:41 -0500

    changes

--- src/Pages/Admin Page/admin.jsx
+++ src/Pages/Admin Page/admin.jsx
@@ -1,1 +1,9 @@
-raf
+import React from 'react'
+
+const admin = () => {
+  return (
+    <div>admin</div>
+  )
+}
+
+export default admin
---
+++ src/Pages/Detailed Page/detailed.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+const detailed = () => {
+  return (
+    <div>detailed</div>
+  )
+}
+
+export default detailed
