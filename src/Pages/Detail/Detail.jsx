032728d3d0b798eac35720edfacbdd9012f7a9bd
Jayviar <jayviarlaporte0@gmail.com>
2023-02-15 11:33:47 -0500

    details!

--- src/Pages/Detail/Detail.jsx
+++ src/Pages/Detail/Detail.jsx
@@ -1,9 +1,38 @@
 import React from 'react'
 
+import './Detail.scss'
 
 const Detail = () => {
   return (
-    <div>detailed</div>
+    <div>
+    <body/> 
+    // Below I made a table to display the patient info and exam info
+    //I want to then 
+           <table class ="table"/>
+        <thead/>
+          <tr>
+            <th scope="col"></th>
+            <th scope="col">Patient Info:</th>
+            <th scope="col">Exam Info:</th>
+          </tr>
+            <thead/>
+            <tbody/>
+            <tr>
+              <th scope='row'></th>
+                <td>Sex:</td>
+                <td>Date:</td>
+                </tr>
+                <th scope='row'></th>
+                <td>Age:</td> 
+                <td>Image:</td>
+                <tr>
+                <th scope='row'></th>
+                <td>BMI:</td>
+                </tr>
+                <tbody/>
+                <table/>
+    <body/>
+    </div>
   )
 }
 
