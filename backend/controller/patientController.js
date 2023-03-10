bd41a36e815091bd1509b8a10872909e70508719
BenAfairi <benedicta_agyemangbrantuo@student.uml.edu>
2023-03-09 20:15:21 -0500

    new changes!!!

--- backend/controller/patientController.js
+++ backend/controller/patientController.js
@@ -4,10 +4,10 @@ const mongoose = require('mongoose')
 // get all patient records in the database
 const getAllRecords = async (req, res) => {
   try {
-    const patients = await Patient.find({}).sort({createdAt: -1})
+    const patients = await Patient.find({}).sort({ createdAt: -1 })
     res.status(200).json(patients)
   } catch (error) {
-    res.status(400).json({error: error.message})
+    res.status(400).json({ error: error.message })
   }
 }
 
@@ -24,12 +24,12 @@ const getPatientRecord = async (req, res) => {
   pngFileName examId icuAdmit numIcuAdmissions mortality'
 
   const patientRecord = await Patient
-    .findOne({PATIENT_ID: PATIENT_ID})
+    .findOne({ PATIENT_ID: PATIENT_ID })
     .select(fieldsToSelect)
 
   // if patient id does not exist a 404 error will be produced
   if (!patientRecord) {
-    return res.status(404).json({error: "Invalid patient ID"})
+    return res.status(404).json({ error: "Invalid patient ID" })
   }
 
   res.status(200).json(patientRecord)
@@ -39,8 +39,10 @@ const getPatientRecord = async (req, res) => {
 // create a new patient record
 // add new patient record to the database
 const createPatientRecord = async (req, res) => {
-  const {name, id} = req.body
-  console.log(name + " " + id)
+  const { name, id } = req.body
+  console.log({ name, id, body: req.body, bodytype: typeof req.body })
+
+  // console.log(name + " " + id)
   console.log(req.body)
 
   try {
@@ -48,7 +50,7 @@ const createPatientRecord = async (req, res) => {
     console.log(patientRecord)
     res.status(200).json(patientRecord)
   } catch (error) {
-    res.status(400).json({error: error.message})
+    res.status(400).json({ error: error.message })
   }
 }
 
@@ -60,15 +62,15 @@ const deletePatientRecord = async (req, res) => {
 
   // check if the input ID value is valid
   if (!mongoose.Types.ObjectId.isValid(id)) {
-    return res.status(404).json({error: 'Invalid patient ID'})
+    return res.status(404).json({ error: 'Invalid patient ID' })
   }
 
-  const patientRecord = await Patient.findOneAndDelete({_id: id})
+  const patientRecord = await Patient.findOneAndDelete({ _id: id })
   console.log(patientRecord)
 
   // if patient id does not exist a 404 error will be produced
   if (!patientRecord) {
-    return res.status(400).json({error: "Invalid patient ID"})
+    return res.status(400).json({ error: "Invalid patient ID" })
   }
 
   res.status(200).json(patientRecord)
@@ -76,20 +78,20 @@ const deletePatientRecord = async (req, res) => {
 
 // update a patient record
 const updatePatientRecord = async (req, res) => {
-    // this gets the `req` parameter above
-    const { id } = req.params
+  // this gets the `req` parameter above
+  const { id } = req.params
 
-      // check if the input ID value is valid
+  // check if the input ID value is valid
   if (!mongoose.Types.ObjectId.isValid(id)) {
-    return res.status(404).json({error: 'Invalid patient ID'})
+    return res.status(404).json({ error: 'Invalid patient ID' })
   }
 
-  const patientRecord = await Patient.findOneAndUpdate({_id: id}, {
+  const patientRecord = await Patient.findOneAndUpdate({ _id: id }, {
     ...req.body
   })
 
   if (!patientRecord) {
-    return res.status(400).json({error: "Invalid patient ID"})
+    return res.status(400).json({ error: "Invalid patient ID" })
   }
 
   res.status(200).json(patientRecord)
